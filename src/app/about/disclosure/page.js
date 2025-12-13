43d8cd61a39d4904cbfcf2e6ad1c16e67c2f54dc
Aathithyan2947 <aathireguraj@gmail.com>
2025-12-13 18:46:10 +0530

    Added: PDF on disclosure page

--- src/app/about/disclosure/page.js
+++ src/app/about/disclosure/page.js
@@ -5,49 +5,49 @@ const sections = [
   {
     title: "General Information",
     items: [
-      { label: "General Information", href: "#" }
+      { label: "General Information", href: "https://drive.google.com/file/d/18cKlGMCQvn0CBH_ANaxy_sQldojT-y-6/view" }
     ]
   },
   {
     title: "Documents and Information",
     items: [
-      { label: "Copies of Affiliation Upgradation and Extension", href: "#" },
-      { label: "Copies of Societies Trust Company Registration", href: "#" },
-      { label: "NOC", href: "#" },
-      { label: "Recognition Certificate", href: "#" },
-      { label: "Valid Building Safety Certificate", href: "#" },
-      { label: "Valid Fire Safety Certificate", href: "#" },
-      { label: "Self Certificate", href: "#" },
-      { label: "DEO Certificate", href: "#" },
-      { label: "Valid Water Health and Sanitation Certificate", href: "#" },
-      { label: "Mandatory Disclosure Details SARAS 4.0", href: "#" },
+      { label: "Copies of Affiliation Upgradation and Extension", href: "https://drive.google.com/file/d/1W4SzwwexfETp3hlcc2N8K_jQBQnp-QX6/view" },
+      { label: "Copies of Societies Trust Company Registration", href: "https://drive.google.com/file/d/1cHFe5yDx8OKrHwg6sq-ai8jSEHvlEi2N/view" },
+      { label: "NOC", href: "https://drive.google.com/file/d/1WuGQ63u3td5stEWluX4Dj9iIMoimAvC1/view" },
+      { label: "Recognition Certificate", href: "https://drive.google.com/file/d/1pRKIkEFTBZ4fyk567SiCLMGubVweILPJ/view" },
+      { label: "Valid Building Safety Certificate", href: "https://drive.google.com/file/d/1hbdCDOKyYmE8YSt0p7fHdKi5kHP0GAJu/view" },
+      { label: "Valid Fire Safety Certificate", href: "https://drive.google.com/file/d/1XhCMrx6XMwX23FbVxLh3kEwwF-h___E7/view" },
+      { label: "Self Certificate", href: "https://drive.google.com/file/d/1ZYMoDpWb6cSt6h0uShg8Wk7J13j_Tnml/view" },
+      { label: "DEO Certificate", href: "https://drive.google.com/file/d/1Vu_GgHHGaiY__R7dNQprNGLifiRT8dyP/view" },
+      { label: "Valid Water Health and Sanitation Certificate", href: "https://drive.google.com/file/d/11fXDAOj3POeWFaas0sWfjwjgGer81nYH/view" },
+      { label: "Mandatory Disclosure Details SARAS 4.0", href: "https://drive.google.com/file/d/1ddEgdgH8dLrBj4DnEH6RIQTsLwmiETMX/view" },
     ]
   },
   {
     title: "Result and Academics",
     items: [
       { label: "Fee Structure of the School", href: "#" },
-      { label: "Annual Academic Calendar", href: "#" },
-      { label: "List of SMC", href: "#" },
-      { label: "Last 3 Years Result of Board Examination", href: "#" },
+      { label: "Annual Academic Calendar", href: "https://drive.google.com/file/d/1trXX17D2VCPKNpFNOuI9k6QK9ZApWlmK/view" },
+      { label: "List of SMC", href: "https://drive.google.com/file/d/1trXX17D2VCPKNpFNOuI9k6QK9ZApWlmK/view" },
+      { label: "Last 3 Years Result of Board Examination", href: "https://drive.google.com/file/d/1_9tMWZdpjril0TR3SJ-Cvuz7h4tWFu6j/view" },
     ]
   },
   {
     title: "Staff Details",
     items: [
-      { label: "Staff Deatils", href: "#" }
+      { label: "Staff Details", href: "https://drive.google.com/file/d/1RSUKTDx0sTIonT-NJaN3WfRTLxRyeWem/view" }
     ]
   },
   {
     title: "School Infrastructure",
     items: [
-      { label: "School Infrastructure", href: "#" }
+      { label: "School Infrastructure", href: "https://drive.google.com/file/d/1q_a-ZC5ltF3b9YMquOoMmWT338sBi4J4/view" }
     ]
   },
   {
     title: "Mandatory Disclosure SARAS",
     items: [
-      { label: "Link to PDF", href: "#" }
+      { label: "Link to PDF", href: "https://drive.google.com/file/d/1UTmjHDyEzKRFgacrouZyz4V7e9Ev-ymy/view" }
     ]
   },
 ];
@@ -78,6 +78,8 @@ export default function Disclosure() {
                 {/* View Button */}
                 <Link
                   href={item.href}
+                  target="_blank"
+                  rel="noopener noreferrer"
                   className="px-6 py-2 border border-gray-400 rounded hover:bg-gray-100 transition"
                 >
                   View
