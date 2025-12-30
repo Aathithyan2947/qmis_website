1e1050bd7b2f247ed405ae0a10ac2e8df46f14e9
Aathithyan2947 <aathireguraj@gmail.com>
2025-12-30 16:02:18 +0530

    Fixed : the right side panel in blog

--- src/app/educational-resources/blog/BlogRightPanel.js
+++ src/app/educational-resources/blog/BlogRightPanel.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function BlogRightPanel() {
   return (
-    <aside className="bg-[#1b2559] text-white overflow-hidden max-w-sm h-[105vh] flex flex-col">
+    <aside className="text-white flex flex-col">
 
       <div className="h-[220px] w-full">
         <Image
@@ -13,7 +13,7 @@ export default function BlogRightPanel() {
         />
       </div>
 
-      <div className="flex-1 px-6 py-12 space-y-4 text-md leading-loose text-white/90">
+      <div className="bg-darkBlue-100 px-6 py-12 space-y-4 text-md leading-loose text-white/90">
         <p>
           With 12+ years of indisputable academic legacy, Queen Mira
           International School is celebrated as one of the best CBSE
@@ -29,13 +29,13 @@ export default function BlogRightPanel() {
           From Academic Struggles to a Holistic Education, witness your
           child's transformational learning journey at Queen Mira.
         </p>
+        <div className="flex justify-center px-6 pt-3 cursor-pointer">
+          <button className="text-maroon-100 bg-white py-2 px-2 rounded-md text-sm font-medium hover:scale-105 duration-200 transition">
+            LEARN MORE
+          </button>
+        </div>
       </div>
 
-      <div className="flex justify-center px-6 pb-6 cursor-pointer">
-        <button className="text-maroon-100 bg-white py-2 px-2 rounded-md text-sm font-medium hover:scale-105 duration-200 transition">
-          LEARN MORE
-        </button>
-      </div>
     </aside>
   );
 }
