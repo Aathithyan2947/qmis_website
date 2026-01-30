79c5b50baee4783a4bb5ce30b4b6063a54578493
Aathithyan2947 <aathireguraj@gmail.com>
2026-01-30 18:26:19 +0530

    Added: new links in the disclosure

--- src/app/about/disclosure/page.js
+++ src/app/about/disclosure/page.js
@@ -5,49 +5,42 @@ const sections = [
   {
     title: "General Information",
     items: [
-      { label: "General Information", href: "https://drive.google.com/file/d/18cKlGMCQvn0CBH_ANaxy_sQldojT-y-6/view" }
+      { label: "General Information", href: "https://drive.google.com/file/d/1lmFhm91rSgdD_NYBdXSi95YZkS5_yIc7/view" }
     ]
   },
   {
     title: "Documents and Information",
     items: [
-      { label: "Copies of Affiliation Upgradation and Extension", href: "https://drive.google.com/file/d/1W4SzwwexfETp3hlcc2N8K_jQBQnp-QX6/view" },
-      { label: "Copies of Societies Trust Company Registration", href: "https://drive.google.com/file/d/1cHFe5yDx8OKrHwg6sq-ai8jSEHvlEi2N/view" },
-      { label: "NOC", href: "https://drive.google.com/file/d/1WuGQ63u3td5stEWluX4Dj9iIMoimAvC1/view" },
-      { label: "Recognition Certificate", href: "https://drive.google.com/file/d/1pRKIkEFTBZ4fyk567SiCLMGubVweILPJ/view" },
-      { label: "Valid Building Safety Certificate", href: "https://drive.google.com/file/d/1hbdCDOKyYmE8YSt0p7fHdKi5kHP0GAJu/view" },
-      { label: "Valid Fire Safety Certificate", href: "https://drive.google.com/file/d/1XhCMrx6XMwX23FbVxLh3kEwwF-h___E7/view" },
-      { label: "Self Certificate", href: "https://drive.google.com/file/d/1ZYMoDpWb6cSt6h0uShg8Wk7J13j_Tnml/view" },
-      { label: "DEO Certificate", href: "https://drive.google.com/file/d/1Vu_GgHHGaiY__R7dNQprNGLifiRT8dyP/view" },
-      { label: "Valid Water Health and Sanitation Certificate", href: "https://drive.google.com/file/d/11fXDAOj3POeWFaas0sWfjwjgGer81nYH/view" },
-      { label: "Mandatory Disclosure Details SARAS 4.0", href: "https://drive.google.com/file/d/1ddEgdgH8dLrBj4DnEH6RIQTsLwmiETMX/view" },
+      { label: "Copies of Affiliation Upgradation and Extension", href: "https://drive.google.com/file/d/1CEHTzBFXM6teLJW2LTlhgrpdhUv7PjF7/view?usp=drive_link" },
+      { label: "Copies of Societies Trust Company Registration", href: "https://drive.google.com/file/d/1ErgYcyNEPLQA579-NsmdGmKq4RrnFZBx/view?usp=drive_link" },
+      { label: "NOC", href: "https://drive.google.com/file/d/1MLbwc1Vv2X8KNS4-LX_UWYjHto1nXe-V/view?usp=drive_link" },
+      { label: "Recognition Certificate", href: "https://drive.google.com/file/d/1CjtRDvS9rE5CDUYLYc7on9uW3WAzj0hb/view?usp=drive_link" },
+      { label: "Valid Building Safety Certificate", href: "https://drive.google.com/file/d/1NZGdOiINvAHm4n4fNGsl853xbPWnux7w/view?usp=drive_link" },
+      { label: "Valid Fire Safety Certificate", href: "https://drive.google.com/file/d/1qYNkzf-TRYmllSSKq3tNN79HdGO22-sx/view?usp=drive_link" },
+      { label: "DEO Certificate", href: "https://drive.google.com/file/d/1fyj8CHWFkHXww3LYzXsjIJI0AGDIf8Ln/view?usp=drive_link" },
+      { label: "Valid Water Health and Sanitation Certificate", href: "https://drive.google.com/file/d/1Ir0xkdUJJT0WFZbNog8LV3raO2PZcDMo/view?usp=drive_link" },
     ]
   },
   {
     title: "Result and Academics",
     items: [
-      { label: "Fee Structure of the School", href: "#" },
-      { label: "Annual Academic Calendar", href: "https://drive.google.com/file/d/1trXX17D2VCPKNpFNOuI9k6QK9ZApWlmK/view" },
-      { label: "List of SMC", href: "https://drive.google.com/file/d/1qc7wT9sVBAwU4NOOf7bIWvjEGGHc-3FV/view" },
-      { label: "Last 3 Years Result of Board Examination", href: "https://drive.google.com/file/d/1_9tMWZdpjril0TR3SJ-Cvuz7h4tWFu6j/view" },
+      { label: "Fee Structure of the School", href: "https://drive.google.com/file/d/1aS3LJPtUXzaRi6kv6pyTesZR1HkS7m17/view?usp=drive_link" },
+      { label: "Annual Academic Calendar", href: "https://drive.google.com/file/d/1fY0-YZn2NbnAXler5U3IM1XYRmUonSEg/view?usp=drive_link" },
+      { label: "List of SMC", href: "https://drive.google.com/file/d/1DzkxiAcfEB0TFTl5S_bLtN90mOZE59fn/view?usp=drive_link" },
+      { label: "List of Parents Teachers Association (PTA) Members", href: "https://drive.google.com/file/d/1xTXWn4Sf3ZWtZvIivHSQ8sIxrghefF1Z/view?usp=drive_link" },
+      { label: "Last 3 Years Result of Board Examination", href: "https://drive.google.com/file/d/1xTXWn4Sf3ZWtZvIivHSQ8sIxrghefF1Z/view?usp=drive_link" },
     ]
   },
   {
     title: "Staff Details",
     items: [
-      { label: "Staff Details", href: "https://drive.google.com/file/d/1RSUKTDx0sTIonT-NJaN3WfRTLxRyeWem/view" }
+      { label: "Staff Details", href: "https://drive.google.com/file/d/1KvZoAf1A5dmy3_z0yZ7uViedWG2twvfa/view?usp=drive_link" }
     ]
   },
   {
     title: "School Infrastructure",
     items: [
-      { label: "School Infrastructure", href: "https://drive.google.com/file/d/1q_a-ZC5ltF3b9YMquOoMmWT338sBi4J4/view" }
-    ]
-  },
-  {
-    title: "Mandatory Disclosure SARAS",
-    items: [
-      { label: "Link to PDF", href: "https://drive.google.com/file/d/1UTmjHDyEzKRFgacrouZyz4V7e9Ev-ymy/view" }
+      { label: "School Infrastructure", href: "https://drive.google.com/file/d/1HcO8E64pfHGG_0I_MxaVIjTucfF6mK4t/view?usp=drive_link" }
     ]
   },
 ];
@@ -59,11 +52,11 @@ export default function Disclosure() {
 
       <div className="bg-grid-dots mx-auto w-full md:max-w-6xl px-10 py-10 text-gray-700">
 
-        <div className="bg-yellow-200 p-3 mb-4 rounded">
-          <p className="text-yellow-900 text-md">
-            Note: This page is currently under development.
-          </p>
-        </div>
+        {/* <div className="bg-yellow-200 p-3 mb-4 rounded"> */}
+        {/*   <p className="text-yellow-900 text-md"> */}
+        {/*     Note: This page is currently under development. */}
+        {/*   </p> */}
+        {/* </div> */}
 
         {sections.map((section, index) => (
           <div key={index} className="mb-12">
@@ -82,14 +75,20 @@ export default function Disclosure() {
                 <p className="text-[18px]">{item.label}</p>
 
                 {/* View Button */}
-                <Link
-                  href={item.href}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="px-6 py-2 border border-gray-400 rounded hover:bg-gray-100 transition"
-                >
-                  View
-                </Link>
+                {item.label === "List of Parents Teachers Association (PTA) Members" ? (
+                  <span className="px-6 py-2 text-gray-500 italic">
+                    Not Available
+                  </span>
+                ) : (
+                  <Link
+                    href={item.href}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="px-6 py-2 border border-gray-400 rounded hover:bg-gray-100 transition"
+                  >
+                    View
+                  </Link>
+                )}
               </div>
             ))}
           </div>
