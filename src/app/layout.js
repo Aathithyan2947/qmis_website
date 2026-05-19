3d8ad1c29154fd149a11e68041273dd6fb3f2e47
Aathithyan2947 <aathireguraj@gmail.com>
2026-05-19 11:50:50 +0530

    Disabled the chatbot

--- src/app/layout.js
+++ src/app/layout.js
@@ -109,7 +109,7 @@ export default function RootLayout({ children }) {
         <MainHeader />
         {children}
         <Toaster />
-        <ChatBot />
+        {/* <ChatBot /> */}
 
         {/* WhatsApp Floating Button */}
         <Link
